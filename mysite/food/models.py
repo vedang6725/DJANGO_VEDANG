9d088d79280dee1e3e0e1db2b5670519a9e7ef85
vedang rane <vedangrane330@gmail.com>
2023-09-04 16:52:40 +0530

    commit:8

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -6,3 +6,9 @@ class Item(models.Model):
     item_name = models.CharField(max_length=50)
     item_desc = models.TextField(max_length=300)
     item_price = models.IntegerField()
+
+
+
+
+
+
--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from food import views
 
 urlpatterns = [
-    path('home/', views.index, name = 'index'),
+    path('index/', views.index, name = 'index'),
     path('detail/', views.detail, name = 'detail'),
 ]

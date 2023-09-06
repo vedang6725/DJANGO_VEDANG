2d30235d223b796cb17cd3abf23a330339dfd24a
vedang rane <vedangrane330@gmail.com>
2023-09-06 16:33:11 +0530

    commit:16

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -3,5 +3,5 @@ from food import views
 
 urlpatterns = [
     path('home/', views.index, name = 'index'),
-    path('detail/', views.detail, name = 'detail'),
+    path('detail/<int:item_id>/', views.detail, name = 'detail'),
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -15,5 +15,5 @@ def index(request):
     return render(request, 'food/index.html', context)
 
 
-def detail(request):
-    return HttpResponse('<h1 style = "color:blue">This is a detail page</h1>')
+def detail(request, item_id):
+    return HttpResponse('item_id: {}'.format(item_id))

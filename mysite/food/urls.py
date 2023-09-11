6231099d3c9e29c5905db2513ed6faf536299bc1
vedang rane <vedangrane330@gmail.com>
2023-09-11 20:35:01 +0530

    commit:20

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from food import views
 
+app_name = 'food'
+
 urlpatterns = [
     path('home/', views.index, name = 'index'),
     path('detail/<int:item_id>/', views.detail, name = 'detail'),

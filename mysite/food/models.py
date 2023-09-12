f084ae4375d303470e25f4cea3bb353bb878df7a
vedang rane <vedangrane330@gmail.com>
2023-09-12 16:36:54 +0530

    commit:23

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -6,12 +6,10 @@ class Item(models.Model):
     item_name = models.CharField(max_length=50)
     item_desc = models.TextField(max_length=300)
     item_price = models.IntegerField()
+    item_image = models.CharField(
+        max_length=500,
+        default="https://rrslide.com/wp-content/uploads/edd/2022/05/Food-Placeholder-Slides-PPT-14-min.jpg"
+    )
 
     def __str__(self):
         return self.item_name
-
-
-
-
-
-

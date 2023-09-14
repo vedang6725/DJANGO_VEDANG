5dbc913881f9b6d6a0dafc3498ea157b75c854e8
vedang rane <vedangrane330@gmail.com>
2023-09-14 16:04:30 +0530

    commit:27

--- mysite/food/models.py
+++ mysite/food/models.py
@@ -4,20 +4,14 @@ from django.db import models
 
 class Item(models.Model):
     prod_code = models.IntegerField(default=100)
-    for_user = models.CharField(
-        max_length=100, 
-        default='xyz'
-    )
+    for_user = models.CharField(max_length=100, default='xyz')
     item_name = models.CharField(max_length=50)
-    item_desc = models.CharField(
-        max_length=500,
-        default='''Lorem, ipsum dolor sit amet consectetur adipisicing elit. A nam voluptate assumenda recusandae officia et consequatur, 
-                   nobis voluptatem incidunt laborum harum doloribus aspernatur iure aperiam adipisci quas alias ea delectus!'''
-    )
+    item_desc = models.CharField(max_length=500,default="Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando")
     item_price = models.IntegerField()
     item_image = models.CharField(
         max_length=500,
-        default="https://th.bing.com/th?id=OIP.N0JNvG4iu61u97rvu8FZWgHaFe&w=290&h=214&c=8&rs=1&qlt=90&o=6&pid=3.1&rm=2"
-    )
-    def __str__(self):
-       return self.item_name
+        default="https://livingstonbagel.com/wp-content/uploads/2016/11/food-placeholder.jpg"
+     )
+    
+    def _str_(self):
+        return self.item_name

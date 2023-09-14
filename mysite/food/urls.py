21cf2d4f2f7e692506d43a657c73f735d160a9f4
vedang rane <vedangrane330@gmail.com>
2023-09-14 16:47:51 +0530

    commit:29

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -6,5 +6,6 @@ app_name = 'food'
 urlpatterns = [
     path('home/', views.index, name = 'index'),
     path('detail/<int:item_id>/', views.detail, name = 'detail'),
+    path('add/', views.create_item, name='create_item'),
     
 ]
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from food.models import Item
+from food.forms import ItemForm
 
 # Create your views here.
 
@@ -24,4 +25,15 @@ def detail(request, item_id):
 
     return render(request, 'food/detail.html', context)
 
+def create_item(request):
+    form = ItemForm(request.POST or None)
 
+    if form.is_valid():
+        form.save()
+        return redirect('food:index')
+
+    context = {
+        'form':form
+    }
+
+    return render(request, 'food/item-form.html', context)

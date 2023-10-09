f638c8abf6102c42601af179f0696dace3738653
Vedang Rane <vedangrane430@gmail.com>
2023-10-09 15:40:36 +0530

    commit:60

--- mysite/food/urls.py
+++ mysite/food/urls.py
@@ -4,10 +4,32 @@ from food import views
 app_name = 'food'
 
 urlpatterns = [
-    path('home/', views.index, name = 'index'),
+
+
+    # function based index view
+    # ----------------------------------------------------
+    #path('home/', views.index, name = 'index'),
+
+    # class based index view
+    # ----------------------------------------------------
+    path('home', views.IndexClassView.as_view(), name = 'index'),
+
+    # function based detail view
+    # ----------------------------------------------------
     path('detail/<int:item_id>/', views.detail, name = 'detail'),
+
+    # function based create item view
+    # ----------------------------------------------------
     path('add/', views.create_item, name='create_item'),
+
+    #function based update item view
+    # ----------------------------------------------------
     path('update/<int:id>/', views.update_item, name='update_item'),
+
+    # function based delete item view
+    # ----------------------------------------------------
     path('delete/<int:id>/', views.delete_item, name='delete_item'),
+
+
 ]
 
--- mysite/food/views.py
+++ mysite/food/views.py
@@ -2,9 +2,13 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from food.models import Item
 from food.forms import ItemForm
+from django.views.generic.list import ListView
 
 # Create your views here.
+# ----------------------------------------------------------------
 
+# function based index view
+# ----------------------------------------------------
 
 def index(request):
     itemlist = Item.objects.all()
@@ -16,6 +20,19 @@ def index(request):
     return render(request, 'food/index.html', context)
 
 
+# class based index view
+# ----------------------------------------------------
+
+class IndexClassView(ListView):
+
+    model = Item
+    context_object_name = 'itemlist'
+    template_name = 'food/index.html'
+
+
+# function based detail view
+# ----------------------------------------------------
+
 def detail(request, item_id):
     item = Item.objects.get(pk=item_id)
 
@@ -24,6 +41,9 @@ def detail(request, item_id):
     }
 
     return render(request, 'food/detail.html', context)
+
+# function based create item view
+# ----------------------------------------------------
 
 def create_item(request):
     form = ItemForm(request.POST or None)
@@ -38,6 +58,9 @@ def create_item(request):
 
     return render(request, 'food/item-form.html', context)
 
+# function based update item view
+# ----------------------------------------------------
+
 def update_item(request, id):
     item = Item.objects.get(pk=id)
     form = ItemForm(request.POST or None, instance=item)
@@ -51,6 +74,9 @@ def update_item(request, id):
         return redirect('food:index')
 
     return render(request, 'food/item-form.html', context)
+
+# function based delete item view
+# ----------------------------------------------------
 
 def delete_item(request, id):
     item = Item.objects.get(pk=id)

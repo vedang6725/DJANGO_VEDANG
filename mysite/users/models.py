78f2be05491312010dda10936067e397dbd8a970
Vedang Rane <vedangrane430@gmail.com>
2023-10-12 16:01:28 +0530

    commit:66

--- mysite/users/models.py
+++ mysite/users/models.py
@@ -7,6 +7,7 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image =  models.ImageField(default='profilepic.jpg', upload_to = 'profile_pictures')
     location = models.CharField(max_length=100)
+    user_type = models.CharField(max_length=200, default='users')
 
     def __str__(self):
         return self.user.username

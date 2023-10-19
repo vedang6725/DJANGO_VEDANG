74891e26f1981d0787d39eeb8ab40f0b59298a28
Vedang Rane <vedangrane430@gmail.com>
2023-10-19 16:36:47 +0530

    commit:78

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -24,6 +24,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('food/', include('food.urls')),
+    path('users/', include('users.urls')),
     path('register/', users_views.register, name='register'),
     path('login/', users_views.login_view, name='login'),
     path('logout/', users_views.logout_view, name='logout'),

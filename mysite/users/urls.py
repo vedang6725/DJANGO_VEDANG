706c7f45504d7b67df0fbcc7990a660c830d68c3
Vedang <vedangrane430@gmail.com>
2023-10-30 16:44:36 +0530

    commit:82

--- mysite/users/urls.py
+++ mysite/users/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     
     # orders
     path('orders/<int:id>/<int:pdcd>/<str:user>/', views.Orders, name='orders'),
+
+    # updating customer orders
+    path('upd_orders/<int:id>/<int:upd_order_id>/', views.update_orders, name="upd_orders")
 ]
